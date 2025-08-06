3daed0ac93f714d4953e52000c5ce1eddc8913e4
BeRi <iberi22@gmail.com>
2025-08-06 04:18:51 -0500

    feat: add path alias configuration for src directory using @

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,21 @@
 
 import yaml from "@rollup/plugin-yaml"
 import { defineConfig } from "astro/config"
+import path from "path"
+import { fileURLToPath } from "url"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://brasaberi-portfolio.vercel.app",
   vite: {
     plugins: [yaml()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
+    },
   },
 })
